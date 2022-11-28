861e471454ff9b063cad9ed6272053574af81c51
Toluadedayo23 <toluadedayo23@gmail.com>
2022-11-28 23:30:07 +0100

    controller

--- src/main/java/com/tutorials/ecommerceapp/controller/CartController.java
+++ src/main/java/com/tutorials/ecommerceapp/controller/CartController.java
@@ -1,11 +1,15 @@
 package com.tutorials.ecommerceapp.controller;
 
 import com.tutorials.ecommerceapp.dto.cart.AddToCartDto;
+import com.tutorials.ecommerceapp.dto.cart.UpdateCart;
+import com.tutorials.ecommerceapp.dto.cart.ViewCartResponse;
 import com.tutorials.ecommerceapp.service.CartService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/cart")
@@ -20,8 +24,25 @@ public class CartController {
     }
 
     @GetMapping()
-    public void viewCart(){
-        cartService.viewCart();
+    public ResponseEntity<ViewCartResponse> viewCart(){
+        return ResponseEntity.status(HttpStatus.OK).body(cartService.viewCart());
     }
 
+    @DeleteMapping("/removeItemFromCart/{id}")
+    public ResponseEntity<String> removeItemFromCart(@PathVariable("id") Long cartId){
+        cartService.removeItemFromCart(cartId);
+        return new ResponseEntity<String>("Item successfully removed", HttpStatus.OK);
+    }
+
+    @PutMapping("/updateCart")
+    public ResponseEntity<String> updateCart(@RequestBody @Valid UpdateCart updateCart){
+        cartService.updateCart(updateCart);
+        return new ResponseEntity<String>("Item successfully updated", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/clearCart")
+    public ResponseEntity<String> clearCart(){
+        cartService.clearCart();
+        return new ResponseEntity<String>("Cart cleared", HttpStatus.OK);
+    }
 }
