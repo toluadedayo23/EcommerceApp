d2b25b9c9d2844e475ae38db6747286f71cc68a7
Toluadedayo23 <toluadedayo23@gmail.com>
2022-11-28 23:31:42 +0100

    dto

--- src/main/java/com/tutorials/ecommerceapp/dto/cart/UpdateCart.java
+++ src/main/java/com/tutorials/ecommerceapp/dto/cart/UpdateCart.java
@@ -1,2 +1,18 @@
-package com.tutorials.ecommerceapp.dto.cart;public class UpdateCart {
+package com.tutorials.ecommerceapp.dto.cart;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class UpdateCart {
+
+    @NotEmpty
+    private Long cartId;
+
+    @NotEmpty
+    private Long productId;
+
+    @NotEmpty
+    private Integer quantity;
 }
--- src/main/java/com/tutorials/ecommerceapp/dto/cart/ViewCartResponse.java
+++ src/main/java/com/tutorials/ecommerceapp/dto/cart/ViewCartResponse.java
@@ -1,11 +1,12 @@
 package com.tutorials.ecommerceapp.dto.cart;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
-
+@AllArgsConstructor
 public class ViewCartResponse {
 
     private List<CartItems> cartItems;
