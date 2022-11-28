51cb4bc69ac53f12cd1af39facd11a5b4fd9d8fd
Toluadedayo23 <toluadedayo23@gmail.com>
2022-11-28 23:32:26 +0100

    Services

--- src/main/java/com/tutorials/ecommerceapp/service/CartService.java
+++ src/main/java/com/tutorials/ecommerceapp/service/CartService.java
@@ -1,6 +1,7 @@
 package com.tutorials.ecommerceapp.service;
 
 import com.tutorials.ecommerceapp.dto.cart.AddToCartDto;
+import com.tutorials.ecommerceapp.dto.cart.UpdateCart;
 import com.tutorials.ecommerceapp.dto.cart.ViewCartResponse;
 
 import java.util.List;
@@ -8,5 +9,11 @@ import java.util.List;
 public interface CartService {
     void addToCart(AddToCartDto addToCartDto);
 
-    List<ViewCartResponse> viewCart();
+    ViewCartResponse viewCart();
+
+    void removeItemFromCart(Long cartId);
+
+    void updateCart(UpdateCart updateCart);
+
+    void clearCart();
 }
--- src/main/java/com/tutorials/ecommerceapp/service/impl/CartServiceImpl.java
+++ src/main/java/com/tutorials/ecommerceapp/service/impl/CartServiceImpl.java
@@ -2,18 +2,21 @@ package com.tutorials.ecommerceapp.service.impl;
 
 import com.tutorials.ecommerceapp.dto.cart.AddToCartDto;
 import com.tutorials.ecommerceapp.dto.cart.CartItems;
+import com.tutorials.ecommerceapp.dto.cart.UpdateCart;
 import com.tutorials.ecommerceapp.dto.cart.ViewCartResponse;
 import com.tutorials.ecommerceapp.exception.CartException;
 import com.tutorials.ecommerceapp.exception.ProductException;
 import com.tutorials.ecommerceapp.mapper.CartMapper;
 import com.tutorials.ecommerceapp.model.Cart;
 import com.tutorials.ecommerceapp.model.Product;
+import com.tutorials.ecommerceapp.model.User;
 import com.tutorials.ecommerceapp.repository.CartRepository;
 import com.tutorials.ecommerceapp.repository.ProductRepository;
 import com.tutorials.ecommerceapp.service.AuthService;
 import com.tutorials.ecommerceapp.service.CartService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
@@ -30,7 +33,7 @@ public class CartServiceImpl implements CartService {
 
     private final CartMapper cartMapper;
 
-
+    @Transactional
     @Override
     public void addToCart(AddToCartDto addToCartDto) {
         Product product = productRepository.findById(addToCartDto.getProductId())
@@ -46,21 +49,56 @@ public class CartServiceImpl implements CartService {
     }
 
     @Override
-    public List<ViewCartResponse> viewCart() {
+    public ViewCartResponse viewCart() {
         List<Cart> carts = cartRepository.findByUser(authService.getCurrentUser());
 
         if (carts.isEmpty()) {
-            throw new CartException("Cart is Empty, please add items");
+            throw new CartException("Cart is Empty, please add items to cart");
         }
 
         List<CartItems> cartItemsList = cartMapper.mapCartToCartItemsList(carts);
+        Double totalCost = 0.0;
 
+        for (Cart cart : carts){
+            totalCost += cart.getQuantity() * cart.getProduct().getPrice();
+        }
+
+        ViewCartResponse response = new ViewCartResponse(cartItemsList, totalCost);
+        return response;
 
     }
 
-    private void calculateTotalCost(List<CartItems> cartItemsList){
-        cartItemsList.forEach(
-                cartItems -> cartItems.getQuantity() *
-        );
+    @Transactional
+    @Override
+    public void removeItemFromCart(Long cartId) {
+        Cart cart = cartRepository.findById(cartId).orElseThrow(
+                () -> new CartException("Cart with cardId: " + cartId + " not found, please make sure cart is selected"));
+
+        cartRepository.delete(cart);
     }
+
+    @Transactional
+    @Override
+    public void updateCart(UpdateCart updateCart) {
+        Product product = productRepository.findById(updateCart.getProductId())
+                .orElseThrow(() -> new ProductException("Product with the ProductId: " +
+                        "" + updateCart.getProductId() + " not found"));
+
+        Cart cart = cartRepository.findById(updateCart.getCartId())
+                .orElseThrow(() -> new CartException("Cart not found, cannot update, please make sure the right one is selected"));
+
+        cart.setQuantity(updateCart.getQuantity());
+        cart.setCreatedDate(new Date());
+        cartRepository.save(cart);
+
+    }
+
+    @Transactional
+    @Override
+    public void clearCart() {
+        User user = authService.getCurrentUser();
+        cartRepository.deleteAllByUser(user);
+    }
+
+
 }
