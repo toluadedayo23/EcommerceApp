db5d6a3ffae87a88ea951c2dc66d9362b4557d42
Toluadedayo23 <toluadedayo23@gmail.com>
2022-12-16 22:57:47 +0100

    Security

--- src/main/java/com/tutorials/ecommerceapp/security/AuthEntryPointJwt.java
+++ src/main/java/com/tutorials/ecommerceapp/security/AuthEntryPointJwt.java
@@ -1,2 +1,25 @@
-package com.tutorials.ecommerceapp.security;public class AuthEntryPointJwt {
+package com.tutorials.ecommerceapp.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class AuthEntryPointJwt implements AuthenticationEntryPoint {
+
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        log.error("Unauthorized error: {}", authException.getMessage());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+    }
+
 }
