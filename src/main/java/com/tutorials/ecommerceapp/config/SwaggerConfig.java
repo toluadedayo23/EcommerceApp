539f0b59fc2785c706f292565803a974babeff9d
Toluadedayo23 <toluadedayo23@gmail.com>
2022-12-16 22:36:28 +0100

    Config

--- src/main/java/com/tutorials/ecommerceapp/config/SwaggerConfig.java
+++ src/main/java/com/tutorials/ecommerceapp/config/SwaggerConfig.java
@@ -1,38 +1,26 @@
 package com.tutorials.ecommerceapp.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
-@EnableSwagger2WebMvc
 public class SwaggerConfig {
     @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(getApiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.lut.ecommerce"))
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI customOpenAPI(@Value("${app.description}") String appDescription,
+                                 @Value("${app.version}") String appVersion) {
+
+        return new OpenAPI().info(new Info()
+                .contact(new Contact().name("Lut").url("http://lut.com").email("contact.lut@gmail.com"))
+                .title("Ecommerce API")
+                .version(appVersion)
+                .description(appDescription).termsOfService("http://swagger.io/terms/")
+                .license(new License().name("Apache 2.0").url("http://www.apache.org/licenses/LICENSE-2.0")));
+
     }
 
-    private ApiInfo getApiInfo() {
-        Contact contact = new Contact("Lut", "http://lut.com", "contact.lut@gmail.com");
-        return new ApiInfoBuilder()
-                .title("Ecommerce API")
-                .description("Documentation For Ecommerce api")
-                .version("1.0.0")
-                .license("Apache 2.0")
-                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
-                .contact(contact)
-                .build();
-    }
 }
--- src/main/java/com/tutorials/ecommerceapp/config/securityConfig.java
+++ src/main/java/com/tutorials/ecommerceapp/config/securityConfig.java
@@ -1,2 +1,70 @@
-package com.tutorials.ecommerceapp.config;public class securityConfig {
+package com.tutorials.ecommerceapp.config;
+
+import com.tutorials.ecommerceapp.security.AuthEntryPointJwt;
+import com.tutorials.ecommerceapp.security.AuthTokenFilter;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.http.HttpMethod.GET;
+
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@AllArgsConstructor
+public class securityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+
+    private final AuthEntryPointJwt unauthorizedHandler;
+
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests()
+                .antMatchers(GET, "/api/product")
+                .permitAll()
+                .antMatchers(GET, "/api/category")
+                .permitAll()
+                .anyRequest().authenticated();
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
 }
