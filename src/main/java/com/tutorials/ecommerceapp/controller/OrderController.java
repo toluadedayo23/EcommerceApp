3b57a1edd088adc4ad0d18356402c92a918e7d16
Toluadedayo23 <toluadedayo23@gmail.com>
2023-01-06 23:19:32 +0100

    controller

--- src/main/java/com/tutorials/ecommerceapp/controller/OrderController.java
+++ src/main/java/com/tutorials/ecommerceapp/controller/OrderController.java
@@ -3,11 +3,14 @@ package com.tutorials.ecommerceapp.controller;
 import com.stripe.exception.StripeException;
 import com.stripe.model.checkout.Session;
 import com.tutorials.ecommerceapp.dto.checkout.CheckoutItemDto;
-import com.tutorials.ecommerceapp.dto.checkout.StripeResponse;
+import com.tutorials.ecommerceapp.dto.order.OrderDto;
 import com.tutorials.ecommerceapp.service.OrderService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -19,9 +22,15 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/create-checkout-session")
-    public ResponseEntity<StripeResponse> checkoutList(@RequestBody List<CheckoutItemDto> checkoutItemDtoList) throws StripeException {
-        Session session = orderService.createSession(checkoutItemDtoList);
-        StripeResponse stripeResponse = new StripeResponse(session.getId());
-        return ResponseEntity.ok().body(stripeResponse);
+    public ResponseEntity<String> checkoutList(@RequestBody List<CheckoutItemDto> checkoutItemDtoList) throws StripeException {
+        Session session = orderService.createCheckoutSession(checkoutItemDtoList);
+        return ResponseEntity.ok().body(session.getId());
+    }
+
+
+    @PostMapping("/create-order")
+    public ResponseEntity<String> createOrder(@RequestBody OrderDto orderDto){
+        orderService.createOrder(orderDto);
+        return ResponseEntity.ok().body("Order has been successfully created");
     }
 }
