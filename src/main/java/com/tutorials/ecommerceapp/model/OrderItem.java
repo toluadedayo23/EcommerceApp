534c109c084f6847d9c65d2d122c8b87607934a9
Toluadedayo23 <toluadedayo23@gmail.com>
2023-01-06 23:43:58 +0100

    model

---
+++ src/main/java/com/tutorials/ecommerceapp/model/OrderItem.java
@@ -0,0 +1,34 @@
+package com.tutorials.ecommerceapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int quantity;
+
+    private Double price;
+
+    private Date createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", referencedColumnName = "orderId")
+    private Order order;
+
+    @OneToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    private Product product;
+}
