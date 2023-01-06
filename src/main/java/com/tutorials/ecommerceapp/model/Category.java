07e1297f07cc46f6e2f4c60cdb1af654994965fb
Toluadedayo23 <toluadedayo23@gmail.com>
2023-01-06 23:43:13 +0100

    model

--- src/main/java/com/tutorials/ecommerceapp/model/Category.java
+++ src/main/java/com/tutorials/ecommerceapp/model/Category.java
@@ -29,7 +29,7 @@ public class Category {
 
     @OneToMany(mappedBy = "category", fetch = FetchType.LAZY,
             cascade = CascadeType.ALL)
-    Set<Product> products;
+    private Set<Product> products;
 
     public Category() {
     }
