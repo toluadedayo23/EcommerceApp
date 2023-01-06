0f7e72dd64da0560ca5cbab0e1bfe7632453bba8
Toluadedayo23 <toluadedayo23@gmail.com>
2023-01-06 23:48:47 +0100

    service

--- src/main/java/com/tutorials/ecommerceapp/service/impl/OrderServiceImpl.java
+++ src/main/java/com/tutorials/ecommerceapp/service/impl/OrderServiceImpl.java
@@ -5,26 +5,46 @@ import com.stripe.exception.StripeException;
 import com.stripe.model.checkout.Session;
 import com.stripe.param.checkout.SessionCreateParams;
 import com.tutorials.ecommerceapp.dto.checkout.CheckoutItemDto;
+import com.tutorials.ecommerceapp.dto.order.OrderDto;
+import com.tutorials.ecommerceapp.exception.OrderException;
+import com.tutorials.ecommerceapp.exception.UserException;
+import com.tutorials.ecommerceapp.model.Cart;
+import com.tutorials.ecommerceapp.model.Order;
+import com.tutorials.ecommerceapp.model.OrderItem;
+import com.tutorials.ecommerceapp.model.User;
+import com.tutorials.ecommerceapp.repository.CartRepository;
+import com.tutorials.ecommerceapp.repository.OrderItemRepository;
+import com.tutorials.ecommerceapp.repository.OrderRepository;
+import com.tutorials.ecommerceapp.repository.UserRepository;
+import com.tutorials.ecommerceapp.service.CartService;
 import com.tutorials.ecommerceapp.service.OrderService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     @Value("${BASE_URL}")
-    private final String baseUrl;
+    private String baseUrl;
 
     @Value("${STRIPE_SECRET_KEY}")
-    private final String apikey;
+    private String apikey;
+
+    private final OrderRepository orderRepository;
+    private final OrderItemRepository orderItemRepository;
+    private final CartRepository cartRepository;
+    private final UserRepository userRepository;
+    private final CartService cartService;
 
     @Override
-    public Session createSession(List<CheckoutItemDto> checkoutItemDtoList) throws StripeException {
+    public Session createCheckoutSession(List<CheckoutItemDto> checkoutItemDtoList) throws StripeException {
 
         String successURL = baseUrl + "payment/success";
 
@@ -34,7 +54,7 @@ public class OrderServiceImpl implements OrderService {
 
         List<SessionCreateParams.LineItem> sessionItemList = new ArrayList<>();
 
-        for (CheckoutItemDto checkoutItemDto: checkoutItemDtoList){
+        for (CheckoutItemDto checkoutItemDto : checkoutItemDtoList) {
             sessionItemList.add(createSessionLineItem(checkoutItemDto));
         }
 
@@ -46,9 +66,56 @@ public class OrderServiceImpl implements OrderService {
                 .addAllLineItem(sessionItemList)
                 .build();
 
-            return Session.create(params);
+        return Session.create(params);
 
     }
+
+    @Transactional
+    @Override
+    public void createOrder(OrderDto orderDto) {
+        User user = userRepository.findById(orderDto.getUserId()).orElseThrow(
+                () -> new UserException("User", orderDto.getUserId()));
+
+        List<Cart> cartList = cartRepository.findByUser(user);
+
+        if(cartList.isEmpty()){
+            throw new OrderException("Can't place order, cart is empty; please add items to cart and checkout to make an order");
+        }
+
+        Order placeOrder = new Order();
+
+        Double cumPrice = 0.0;
+
+        for(Cart cartItems : cartList){
+            cumPrice += (cartItems.getQuantity() * cartItems.getProduct().getPrice());
+        }
+
+        placeOrder.setTotalPrice(cumPrice);
+        placeOrder.setCreatedDate(new Date());
+        placeOrder.setSessionId(orderDto.getSessionId());
+        placeOrder.setUser(user);
+
+        Order createdOrder = orderRepository.save(placeOrder);
+
+        saveOrderItems(cartList, createdOrder);
+
+        cartRepository.deleteAllByUser(user);
+
+    }
+
+    private void saveOrderItems(List<Cart> cartList, Order order){
+
+        for(Cart cartItem: cartList){
+            OrderItem  newOrderItems = new OrderItem();
+            newOrderItems.setQuantity(cartItem.getQuantity());
+            newOrderItems.setPrice(cartItem.getProduct().getPrice());
+            newOrderItems.setCreatedDate(new Date());
+            newOrderItems.setOrder(order);
+            newOrderItems.setProduct(cartItem.getProduct());
+            orderItemRepository.save(newOrderItems);
+        }
+    }
+
 
     private SessionCreateParams.LineItem createSessionLineItem(CheckoutItemDto checkoutItemDto) {
         return SessionCreateParams.LineItem.builder()
@@ -66,6 +133,16 @@ public class OrderServiceImpl implements OrderService {
                                 .setName(checkoutItemDto.getProductName())
                                 .build()
                 ).build();
+    }
+
+    @Override
+    public void getAllOrders(Long userId){
+
+    }
+
+    @Override
+    public void getOrder(Long orderId) {
+
     }
 
 
