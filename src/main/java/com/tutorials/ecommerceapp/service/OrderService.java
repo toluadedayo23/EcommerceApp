3f32cfb40424143eb88c94d97f73bc3229932314
Toluadedayo23 <toluadedayo23@gmail.com>
2023-01-06 23:48:25 +0100

    service

--- src/main/java/com/tutorials/ecommerceapp/service/OrderService.java
+++ src/main/java/com/tutorials/ecommerceapp/service/OrderService.java
@@ -3,10 +3,17 @@ package com.tutorials.ecommerceapp.service;
 import com.stripe.exception.StripeException;
 import com.stripe.model.checkout.Session;
 import com.tutorials.ecommerceapp.dto.checkout.CheckoutItemDto;
+import com.tutorials.ecommerceapp.dto.order.OrderDto;
 
 import java.util.List;
 
 public interface OrderService {
 
-    Session createSession(List<CheckoutItemDto> checkoutItemDtoList) throws StripeException;
+    Session createCheckoutSession(List<CheckoutItemDto> checkoutItemDtoList) throws StripeException;
+
+    void createOrder(OrderDto orderDto);
+
+    void getAllOrders(Long userId);
+
+    void getOrder(Long orderId);
 }
