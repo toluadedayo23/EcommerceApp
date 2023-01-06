c0b8dd4d921140888d87cc12dbdb0a8bd27b222a
Toluadedayo23 <toluadedayo23@gmail.com>
2023-01-06 23:43:40 +0100

    order

--- src/main/java/com/tutorials/ecommerceapp/model/Order.java
+++ src/main/java/com/tutorials/ecommerceapp/model/Order.java
@@ -1,4 +1,34 @@
 package com.tutorials.ecommerceapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "orders")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    private Date createdDate;
+
+    private Double totalPrice;
+
+    private String sessionId;
+
+    @OneToMany(fetch = FetchType.LAZY, orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
 }
