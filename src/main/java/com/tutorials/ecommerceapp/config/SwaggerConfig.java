5c6a4b0ed06e723b56c6b43a7b6d7ff66c123c2d
Toluadedayo23 <toluadedayo23@gmail.com>
2023-02-26 09:53:21 +0100

    Swagger config

--- src/main/java/com/tutorials/ecommerceapp/config/SwaggerConfig.java
+++ src/main/java/com/tutorials/ecommerceapp/config/SwaggerConfig.java
@@ -11,6 +11,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${app.description}") String appDescription,
+                                 @Value("${app.version}") String appVersion) {
 
+        return new OpenAPI().info(new Info()
+                .contact(new Contact().name("Lut").url("http://lut.com").email("contact.lut@gmail.com"))
+                .title("Ecommerce API")
+                .version(appVersion)
+                .description(appDescription).termsOfService("http://swagger.io/terms/")
+                .license(new License().name("Apache 2.0").url("http://www.apache.org/licenses/LICENSE-2.0")));
 
+    }
 }
