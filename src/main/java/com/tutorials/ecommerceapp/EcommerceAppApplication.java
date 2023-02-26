f398a0fd5763b990cef36b5e631b78629e4e31e7
Toluadedayo23 <toluadedayo23@gmail.com>
2023-02-26 09:54:52 +0100

    app class

--- src/main/java/com/tutorials/ecommerceapp/EcommerceAppApplication.java
+++ src/main/java/com/tutorials/ecommerceapp/EcommerceAppApplication.java
@@ -1,7 +1,13 @@
 package com.tutorials.ecommerceapp;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
