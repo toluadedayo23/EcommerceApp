18a62c9728979caeb9098ce62aacf0970b10fec6
Toluadedayo23 <toluadedayo23@gmail.com>
2023-02-26 09:42:11 +0100

    AuthService

--- src/main/java/com/tutorials/ecommerceapp/service/AuthService.java
+++ src/main/java/com/tutorials/ecommerceapp/service/AuthService.java
@@ -1,9 +1,9 @@
 package com.tutorials.ecommerceapp.service;
 
-import com.tutorials.ecommerceapp.dto.AuthenticationResponse;
-import com.tutorials.ecommerceapp.dto.LoginRequest;
-import com.tutorials.ecommerceapp.dto.RefreshTokenRequest;
-import com.tutorials.ecommerceapp.dto.SignupRequest;
+import com.tutorials.ecommerceapp.dto.auth.AuthenticationResponse;
+import com.tutorials.ecommerceapp.dto.auth.LoginRequest;
+import com.tutorials.ecommerceapp.dto.auth.RefreshTokenRequest;
+import com.tutorials.ecommerceapp.dto.auth.SignupRequest;
 import com.tutorials.ecommerceapp.model.User;
 
 public interface AuthService {
